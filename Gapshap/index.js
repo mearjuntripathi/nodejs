2a41cb4d88b1f1e65d349fd65165a8868342e998
Anike567 <ka344057@gmail.com>
2023-06-13 00:09:40 +0530

    modified

--- Gapshap/index.js
+++ Gapshap/index.js
@@ -1,18 +1,29 @@
 const express=require('express');
-
+const bodyparser=require('body-parser');
 
 const app=express();
 
 app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:false}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/html/login.html");
 });
-
 app.get("/signup",function(req,res){
     res.sendFile(__dirname+"/html/signup.html");
+});
+app.post("/login",function(req,res){
+    console.log(req.body.mail);
+    console.log(req.body.password)
 })
 
+app.post("/signup",function(req,res){
+    console.log(req.body.mail);
+    console.log(req.body.password)
+})
+
+
+
 app.listen(3000,function(){
-    console.log("server is running on port http://localhost:3000");
+    console.log("server is running on port 3000");
 })
