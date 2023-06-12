d8971d70a98f76bbf8c03f3d9431e4a0f23985a1
Anike567 <ka344057@gmail.com>
2023-06-12 19:31:58 +0530

    signup and login page added

--- Gapshap/index.js
+++ Gapshap/index.js
@@ -6,8 +6,12 @@ const app=express();
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/html/index.html");
+    res.sendFile(__dirname+"/html/login.html");
 });
+
+app.get("/signup",function(req,res){
+    res.sendFile(__dirname+"/html/signup.html");
+})
 
 app.listen(3000,function(){
     console.log("server is running on port 3000");
